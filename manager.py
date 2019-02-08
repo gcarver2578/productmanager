ed3f327d1dc86d3c64a3f2712dd20f94ab2ca948
Gabriel Carver <37904423+gcarver2578@users.noreply.github.com>
2019-02-08 12:39:54 -0600

    Create manager.py

---
+++ manager.py
@@ -0,0 +1,14 @@
+# Gabriel Carver, Kenneth Goodman, and Kaleb Charles
+# This program is for analyzing data on products and potential sales
+import random
+import graphics as g
+def main():
+  prediction()
+  
+  
+def prediction():
+return
+
+
+main()
+  
