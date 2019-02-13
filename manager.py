918826bb7230023b6b5da763f4a16efb111bc233
Gabriel Carver <37904423+gcarver2578@users.noreply.github.com>
2019-02-13 06:17:41 -0600

    Update manager.py

--- manager.py
+++ manager.py
@@ -1,7 +1,7 @@
 # Gabriel Carver, Holden Stringfield, Eli Horton
 # This program is for analyzing data on products and potential sales
-import random
 import graphics as g
+import pandas as pd
 
 
 def main():
@@ -9,6 +9,15 @@ def main():
   
   
 def prediction():
+    p_win = g.GraphWin('Prediction Graph', 100, 100)
+    p_win.close()
+    file = 'Products.xlsx'
+    data = pd.read_excel(file)
+    # data.set_index('2017', inplace=True)
+    # print(list(data.loc['Paper', :].values))
+    # new_val = [list(data.loc[x, :].values) for x in range(10)]
+    # print(new_val)
+    print(data)
     return
 
 
